e70f535df0db0bffc37b2b41cc65c3b95f907935
Diya Raj <diya.r71414@gmail.com>
2025-04-09 13:27:16 -0400

    added p1 stuff

--- test_calculator.py
+++ test_calculator.py
@@ -12,10 +12,14 @@ class TestCalculator(unittest.TestCase):
 
     # Partner 1
     def test_multiply(self): # 3 assertions
-        pass
+        self.assertEqual(multiply(2, 2), 4)
+        self.assertEqual(multiply(-2, 3), -6)
+        self.assertEqual(multiply(0, 0), 0)
 
     def test_divide(self): # 3 assertions
-        pass
+        self.assertEqual(divide(4, 2), 2)
+        self.assertEqual(divide(-4, 2), 0)
+        self.assertNotEqual(divide(4, 0), 0)
     ##########################
 
     # Partner 2
@@ -38,17 +42,21 @@ class TestCalculator(unittest.TestCase):
         # call log function inside, example:
         # with self.assertRaises(<INSERT_ERROR_TYPE>):
         #     logarithm(0, 5)
-        pass
+        self.assertRaises(ValueError, logarithm, 0, 5)
 
     def test_hypotenuse(self): # 3 assertions
-        pass
+        self.assertEqual(hypotenuse(3, 4), 5)
+        self.assertNotEqual(hypotenuse(5, 4), 5)
+        self.assertNotEqual(hypotenuse(-4, 5), 5)
 
     def test_sqrt(self): # 3 assertions
         # Test for invalid argument, example:
         # with self.assertRaises(<INSERT_ERROR_TYPE>):
         #    square_root(NUM)
         # Test basic function
-        pass
+        self.assertEqual(square_root(1), 1)
+        self.assertEqual(square_root(4), 2)
+        self.assertRaises(ValueError, square_root, -3)
     ##########################
 
 # Do not touch this
