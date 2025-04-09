d5e1b6f3d2dfd9727984dae96e8c228805e2ad16
Mendoza-D1 <mendoza.d@ufl.edu>
2025-04-09 12:37:10 -0400

    modified calculator p1

--- calculator.py
+++ calculator.py
@@ -1,3 +1,4 @@
+import math
 """
 calculator.py
 - Defines functions used to create a simple calculator
@@ -6,6 +7,24 @@ One function per operation, in order.
 """
 # First example
 def add(a, b): 
-    pass
+    a + b
+
+def sub(a, b):
+    a - b
+
+def mul(a, b):
+    a * b
+
+def div(a, b):
+    b/a
+    raise ZeroDivisionError if a == 0 else b/a
+
+def log(a, b):
+    log(a, b)
+    raise ValueError if b == 0 else log(a, b)
+
+def exp(a, b):
+    a ** b
+
 
 
